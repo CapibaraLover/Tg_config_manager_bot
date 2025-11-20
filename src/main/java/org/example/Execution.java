3d54af7b2ddc14bc92542c4396a1b790f1ff2b0f
CapibaraLover <merzlikin.0404@mail.ru>
2025-11-20 15:11:50 +0300

    добавил объявление токена

--- src/main/java/org/example/Execution.java
+++ src/main/java/org/example/Execution.java
@@ -1,4 +1,7 @@
 package org.example;
+
+import org.example.bot.BotLogic;
+
 public class Execution {
     public static void main(String[] args) {
 
---
+++ src/main/java/org/example/bot/BotLogic.java
@@ -0,0 +1,8 @@
+package org.example.bot;
+
+import org.example.Execution;
+
+public class BotLogic {
+    final static private String BOT_TOKEN = System.getenv("BOT_TOKEN");
+
+}
